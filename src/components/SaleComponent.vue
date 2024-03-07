5df4850dc27e840d7eebb5aba8e1153a4ccc2038
ars-bunjoja <abdul.rehman.shahid@bunjoja.com>
2024-03-07 18:01:50 +0500

    To1

--- src/components/SaleComponent.vue
+++ src/components/SaleComponent.vue
@@ -4,7 +4,7 @@
         <div class="card">
             <h4 style="text-align: center;">Enter Data<br>Here</h4>
             <input type="text" placeholder="Sale" id="sale" v-model="sale">
-            <button @click="t()">Submit</button>
+            <button @click="get_consumption()">Submit</button>
         </div>
         <table class="products">
             <tr>
@@ -16,6 +16,19 @@
                 <td>{{ product.name }}</td>
                 <td>{{ product.price }}</td>
                 <td><input type="number" value="0" v-model="product.qty"></td>
+            </tr>
+        </table>
+        <hr>
+        <table class="products" v-if="items.length > 0">
+            <tr>
+                <th>Item</th>
+                <th>Price</th>
+                <th>Consumption</th>
+            </tr>
+            <tr class="product" v-for="item in items" :key="item.id">
+                <td>{{ item.name }}</td>
+                <td>{{ item.price }}</td>
+                <td>{{ item.consumption }}</td>
             </tr>
         </table>
     </div>
@@ -42,6 +55,7 @@
     background: rgba(245, 245, 245, 0.5);
     backdrop-filter: blur(5px);
 }
+
 .com .card button {
     background-color: #42b983;
     padding: 8px 12px;
@@ -49,6 +63,7 @@
     margin: 8px 0px;
     border: none;
 }
+
 .com .card input {
     padding: 8px 12px;
     border-radius: 4px;
@@ -86,6 +101,8 @@ export default {
     data() {
         return {
             products: [],
+            items: [],
+            pRatio: [],
             sale: 0
         }
     },
@@ -115,8 +132,36 @@ export default {
                     qty: product.qty
                 }
             });
-            if(!this.sale > 0) { document.getElementById("sale").style.border = "1px solid red"; return; }
-            console.log({products, sale: this.sale});
+            if (!this.sale > 0) { document.getElementById("sale").style.border = "1px solid red"; return; }
+            return { products: products, sales: this.sale };
+        },
+
+        get_consumption: async function () {
+            this.items = [];
+            let data = JSON.stringify(this.t());
+            await fetch('http://localhost:8653/api/consumption/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: data
+            }).then(
+                res => res.json()
+            ).then(
+                data => {
+                    this.items = data.data.items;
+                    this.pRatio = data.data.products;
+                }
+            ).then(
+                () => {
+                    this.pRatio.map(x => {
+                        ({
+                            ratio: x.ratio * 100
+                        })
+                    })
+                    console.log(this.pRatio);
+                }
+            )
         }
     },
 
